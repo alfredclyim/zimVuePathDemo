16356a7c3d1d3ec78a55b275932930ae0411f1cb
Alfred <alfred@Alfreds-MacBook-Pro.local>
2019-01-30 22:06:51 +0800

    Add Physics Library

--- src/pages/pathDemo.vue
+++ src/pages/pathDemo.vue
@@ -6,6 +6,11 @@
 
 export default {
   name: 'path-demo',
+  data () {
+    return {
+      frame: null
+    }
+  },
   methods: {
     init () {
       var scaling = "fit"; // fit scales to fit the browser window while keeping the aspect ratio
@@ -21,16 +26,27 @@ export default {
       window.purple = zim.purple
       */
 
-      window.frame = new zim.Frame(scaling, width, height); // see docs for more options and info
-      frame.on("ready", function() {
+      this.frame = new zim.Frame(scaling, width, height); // see docs for more options and info
+      window.frame = this.frame
+      this.frame.on("ready", () => {
         zog("ready from ZIM Frame");
 
-        var stage = frame.stage;
-        var stageW = frame.width;
-        var stageH = frame.height;
-        frame.color = frame.yellow;
-        frame.outerColor = frame.dark;
+        var stage = this.frame.stage;
+        var stageW = this.frame.width;
+        var stageH = this.frame.height;
+        this.frame.color = this.frame.yellow;
+        this.frame.outerColor = frame.dark;
         
+        let _container1 = new zim.Container(0, 0, 100, 100)
+        let _container2 = new zim.Container(0, 0, 100, 100)
+
+        stage.addChild(_container1, _container2)
+        _container1.outline()
+        _container2.outline()
+
+        _container1.transform()
+
+        console.log(_container1.hitTestBounds(_container2))
         
         var path = new zim.Squiggle({
           points: [[-169,115.7,0,0,100,0,50,0,"mirror"],[50.1,-36.3,0,0,194.4,11.3,-194.4,-11.3,"mirror"],[141.2,126.7,0,0,-112.1,0,112.1,0,"mirror"],[193.2,-158.4,0,0,186.4,-19.3,-186.4,19.3,"mirror"],[395.6,135.7,0,0,-70.7,4.8,70.7,-4.8,"mirror"],[519.3,-12.2,0,0,184.8,14.5,-184.8,-14.5,"mirror"],[652.9,120.9,0,0,-102.4,40.2,102.4,-40.2,"mirror"]],
